4da55e0c1a34c12b6ddd4495258ecc69b8aa57ce
KimiaProg <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-10-18 10:17:24 +0200

    eje

---
+++ ejercicio2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    //Recuperando la informacion del fichero
+    $file = fopen("locations.csv", "r");
+    do {
+        echo fgetcsv($file);
+        echo "<br>";
+    } while (fgetcsv($file) != false);
+    fclose($file);
+
+    $file = fopen("locations.csv", "a+");
+    /*$arrayDatos= array('Kimia','23','Sevilla');
+    fputscsv($file,$arrayDatos);*/
+
+    
+    ?>
+</body>
+</html>
